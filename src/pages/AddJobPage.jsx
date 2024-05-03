16c848fe97503389e6c47033c4ad28b268a9776d
Brent Machado <brentmachado7@gmail.com>
2024-05-03 11:38:21 -0700

    using toastify to show success messages

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
+import { toast} from 'react-toastify'
 
 const addJobPage = ({ addJobSubmit}) => {
     const [title, setTitle] = useState('')
@@ -33,6 +34,8 @@ const addJobPage = ({ addJobSubmit}) => {
         }
         addJobSubmit(newJob)
         
+        toast.success('Job Added Successfully')
+
         return navigate('/jobs')
         
     }
