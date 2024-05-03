4f1b9ed48118b0cf81062a69c955be0907c42479
Brent Machado <brentmachado7@gmail.com>
2024-05-02 20:50:42 -0700

    create form content and set up states for each input, still need post request implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
                         >Jobs
                       </NavLink>
                       <NavLink
-                        to="/add-jobs"
+                        to="/add-job"
                         className={ linkClass}
                         >Add Job
                       </NavLink>
